d3bca914d689355595da9bb85a35ed69724ac753
yeraydominguez <yeray.dominguez.dev@gmail.com>
2024-12-13 21:13:05 +0000

    java 23

--- src/test/java/dev/yeray/sp/controller/ClientControllerTest.java
+++ src/test/java/dev/yeray/sp/controller/ClientControllerTest.java
@@ -12,7 +12,6 @@ import java.util.Arrays;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import dev.yeray.sp.model.dto.ClientDTO;
@@ -24,7 +23,6 @@ class ClientControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
     private ClientService clientService;
 
     @Test

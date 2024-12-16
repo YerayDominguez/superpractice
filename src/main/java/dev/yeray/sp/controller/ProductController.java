234f509102f560f49264477794ce8c5d86e94044
yeraydominguez <yeray.dominguez.dev@gmail.com>
2024-12-16 10:51:23 +0000

    cors

--- src/main/java/dev/yeray/sp/controller/ProductController.java
+++ src/main/java/dev/yeray/sp/controller/ProductController.java
@@ -24,7 +24,7 @@ import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-@CrossOrigin(origins = "http://localhost:8081")
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/products")
 public class ProductController {
